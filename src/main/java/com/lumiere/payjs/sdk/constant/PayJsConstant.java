6f9b9b9e0abc0370f32ebd98c15c648818a05f16
wecantstop <13520773230@163.com>
2019-01-02 15:01:11 +0800

    添加接口返回状态常量

---
+++ src/main/java/com/lumiere/payjs/sdk/constant/PayJsConstant.java
@@ -0,0 +1,26 @@
+package com.lumiere.payjs.sdk.constant;
+
+/**
+ * @author xuzhiguang
+ * @date 2019/1/2
+ */
+public class PayJsConstant {
+
+    /**
+     * 返回状态码
+     */
+    public static class ReturnCode {
+
+        /**
+         * 成功
+         */
+        public static final int SUCCESS = 1;
+
+        /**
+         * 失败
+         */
+        public static final int FAIL = 0;
+
+    }
+
+}
